1eb1ba1bffde255f60e0ed38e535bb103c2c0963
vijy45 <vijaylpatil45@gmail.com>
2024-01-09 11:06:03 +0530

    project body

---
+++ Health_insurance_project/src/main/java/com/health/main/HealthInsuranceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.health.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthInsuranceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthInsuranceProjectApplication.class, args);
+	}
+
+}
---
+++ Health_insurance_project/src/test/java/com/health/main/HealthInsuranceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.health.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthInsuranceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
